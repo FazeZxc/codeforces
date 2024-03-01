9374ed36eccc19af347500c5c781d9496b782951
FazeZxc <randomuploader.en@gmail.com>
2024-03-01 23:50:05 +0530

    2

---
+++ A_WayTooLongWords.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    string s = "";
+    cin >> t;
+    while (t--)
+    {
+        cin >> s;
+        int l = s.length();
+        if (l > 10)
+        {
+            cout << s[0] << l - 2 << s[l - 1] << endl;
+        }
+        else
+        {
+            cout << s << endl;
+        }
+    }
+    return 0;
+}
